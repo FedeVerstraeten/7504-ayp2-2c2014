d80c4576636b07263a6dd5d530e6612f9b62ed7f
Federico Verstraeten <federico.verstraeten@gmail.com>
2014-05-16 20:51:33 -0300

    FedericoV: Continu? con el desarrollo de la clase NetworkElement

--- TP1/NetworkElementClass.hpp
+++ TP1/NetworkElementClass.hpp
@@ -1,26 +1,54 @@
+#ifndef NETWORK_ELEMENT_CLASS_HPP
+#define NETWORK_ELEMENT_CLASS_HPP
+
+#include<iostream>
+
 class NetworkElement
 {
 	private:
-		
+
 		string name;
+		string type;
 		NetworkElement *father_;
 		NetworkElement *sons[];
-		size_t cantSons;
+		size_t numberSons;
 
 	public:
 
+        /*********************** CONSTRUCTORES *********************************/
 		NetworkElement();
+		NetworkElement(const string,const string);
+
 		// ¿Debería poder copiarse los nodos?
 		// Constructor por copia:
-		//NetworkElement(const NetworkElement&);
-		~NetworkElement();
-	
-		void setName(string n) {name=n;};
-		const string getName()const {return info;}
+		NetworkElement(const NetworkElement&);
+		~NetworkElement() { }
+
+        /*************************** SET & GET *********************************/
+
+		void setName(string n) {name=n;}
+
+		const string getName()const {return name;}
+		const string getType()const {return type;}
 		const NetworkElement *getFather()const {return father;}
-		const NetworkElement *getSons()const {;}
-/*		
+		const NetworkElement *getSons()const {return sons;}
+		const size_t getNumberSons() const {return numberSons;}
+		// getSons: (?) Retorna el puntero al arreglo de punteros a NetworkElement hijos
+
+		/************************** OPERADORES *********************************/
+
+        NetworkElement& operator = (const NetworkElement &); // operador asignacion
+        bool operator ==(const NetworkElement&) const; // operador comparacion
+
+        /************************** MÉTODOS ************************************/
+
+        NetworkElement& connectToElement(NetworkElement&);
+
+
+/*
+
 		void SetIzq( Nodoarbol *i){izq=i;}
 		void SetDer( Nodoarbol *d){der=d;}
-	*/	
+	*/
 };
+ #endif
