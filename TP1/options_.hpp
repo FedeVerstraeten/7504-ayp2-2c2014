51c8e58634035bd5775bd844af18ff0726aaf8b3
Charlie <charlieromano@gmail.com>
2014-05-20 20:45:30 -0300

    Charlie: options

---
+++ TP1/options_.hpp
@@ -0,0 +1,8 @@
+#ifndef OPTIONS_HPP_INCLUDED
+#define OPTIONS_HPP_INCLUDED
+//HOLA
+void opt_input(string const &);
+void opt_output(string const &);
+void opt_help(string const &);
+
+#endif // OPTIONS_HPP_INCLUDED
