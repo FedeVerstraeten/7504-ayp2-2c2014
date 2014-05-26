0bfd7c891a32a76ac7e0dc57b16658b8eafbf8ce
Charlie <charlieromano@gmail.com>
2014-05-26 03:29:57 -0300

    Charlie: commit.

--- TP1/main_TP1.cpp
+++ TP1/main_TP1.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <cstring>
 #include"file_load.hpp"
 #include"common.hpp"
 #include "printers.hpp"
@@ -12,6 +13,7 @@
 #include "cmdline.h"
 #include "options.hpp"
 
+    //void stringMatrix(string *** matrix, size_t rows, size_t columns);
 
 using namespace std;
 
@@ -29,100 +31,51 @@ int main(int argc,char *argv[])
 	cmdline cmdl(options);
 	cmdl.parse(argc, argv);
 
-  string **lines; //este lo usa loadFileMemory()
+    string **lines; //este lo usa loadFileMemory()
 	size_t number_lines; //este lo usa loadFileMemory()
-	status_t f_, status; //estos se usan
+	status_t f_; //estos se usan
 	string st1, st2;
 
-	if(*iss==ifs){
-	f_=loadFileMemory( ifs , &lines , number_lines );
-	if(f_==OK)
+	if(*iss==ifs)
 	{
-		cout<<"piola"<<endl;
+        f_=loadFileMemory( ifs , &lines , number_lines );
+        if(f_==OK)
+        {
+            cout<<"piola"<<endl;
 
-	}
-	close_all_stream_file( ifs , ofs );
-		eraseFileMemory( &lines , number_lines );
-	}
-	else if(*iss==cin){
-
-	inputFromConsole();
-//	cout << net_name << endl;
-	//printElements( *number_lines , cout );
-
-	}
-
-
-
-
-
-
-/*		for( size_t i=1 ; i<number_lines ; i++ )
-		{
-            status = processLine( (*lines[i]), st1, st2);
-            if(status==OK_NETWORK_TYPE)
-            {
-                cout << st1 << st2 << endl;
-                NetworkElement Net0[i](st1,st2);
-                Net0.showContent();
-                cout<<"FLAG"<<endl;
-                //Crear objeto (aux[0]=name, aux[1]=type);
-                //Asignar al vector de Objetos
-            }
-            if(status==OK_NETWORK_NAME)//aca se supone que ya se crearon TODOS los objetos necesarios
-            {
-                cout<<"FLAG 2"<<endl;
-                //buscar name 1
-                //buscar name 2
-                //Estan OK los nombres? (si estan mal, destruir todo y salir)
-                //Crear conexiones(aux[0]=name1, aux[1]=name2);
-            }
-            if(status==ERROR_TEXT_LINE_INVALID)
-            {
-                cout<<"FLAG 3"<<endl;
-                //Delete todo lo creado para que no haya fugas de memoria
-                //Imprimo errores
-                //final del programa
-            }
-		}
-
-
-	//	printElements( number_of_elements , file_out );
-		close_all_stream_file( file_in , file_out );
-		eraseFileMemory( &lines , number_lines );
-	}
-
-	if(f_==OK_INPUT_CIN)
+        }
+        else
+        {
+                close_all_stream_file( ifs , ofs );
+                eraseFileMemory( &lines , number_lines );
+                cout << "nopiola"<<endl ;
+        }
+    }
+	else if(*iss==cin)
 	{
-	    string aux, line;
-	    string net_name;
-	    getline(cin, line);
-        istringstream iss(line);
-        iss >> aux;
-
-            if(aux == "NetworkName")
-            {
-                status = OK_NETNAME;
-                iss >> aux;
-                net_name=aux;
-            }
-            else
-            {
-                cout << "error: missing NetworkName" << "\n";
-                return ERROR_NETNAME;
-            }
-
-
         inputFromConsole();
-        cout << net_name << endl;
-//        printElements( number_of_elements , cout );
 	}
 
-	else
-	{
-        printErrorMessage(f_,cout);
-        return ERROR_ARG;
-	}*/
+    char * cstr[number_lines];
+    for(size_t i=0 ; i<number_lines ; i++)
+    {
+        cstr[i] = new char [(*lines[i]).length()+1];
+        strcpy (cstr[i], (*lines[i]).c_str());
+    }
 
-return 0;
+    #define MAX_WORDS_PER_LINE_DEFAULT 3
+    string matrix[number_lines][MAX_WORDS_PER_LINE_DEFAULT];
+    for(size_t i=0 ; i<number_lines ; i++)
+    {
+      char * p = strtok (cstr[i]," ");
+      size_t j=0;
+      while (p!=0)
+      {
+        matrix[i][j++]=string(p);
+        p = strtok(NULL," ");
+
+      }
+    }
+   //cout << matrix [0][1] << endl;
+   return 0;
 }
