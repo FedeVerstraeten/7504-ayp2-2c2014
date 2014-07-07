5820b31a2b8df7b45af70805f6c97b888ba47b6f
Federico Verstraeten <federico.verstraeten@gmail.com>
2014-07-07 00:54:28 -0300

    FedericoV: Subida de primer versi?n de prueba de tabla hash. La misma funciona, pero no ha sido adaptada al trabajo en el TP2

---
+++ hashing/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "common.hpp"
+#include "TableHash.hpp"
+#include "NodeTable.hpp"
+#include "TypeElement.hpp"
+
+int main(void)
+{
+    TableHash t_hash(3);
+    TypeElement elemX;
+    TypeElement elem1("1","uno"),elem2("2","dos"),elem3("3","tres"),elem123("123","ciento-veintitres");
+    TypeElement elemB("B","Letra-B"), elemWord("hola","HOLA");
+
+    /* Inserto elementos a la tabla */
+    t_hash.insert(elem1);
+    t_hash.insert(elem2);
+    t_hash.insert(elem3);
+    t_hash.insert(elem123);
+    t_hash.insert(elemB);
+    t_hash.insert(elemWord);
+
+    /* Imprimo los elementos */
+    cout<<"nombre elemento"<<endl;
+    t_hash.print(elem1);
+    t_hash.print(elem3);
+    t_hash.print(elem2);
+    t_hash.print(elemB);
+
+    /* Imprimo toda la tabla hash */
+    t_hash.printTable();
+
+    elemX=t_hash.find("hola");
+    cout<<elemX.getName()<<endl;
+
+    return 0;
+}
