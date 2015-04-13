9c95d758e5c7c1e6276bad40108b0b48ae2a0598
Charlie Romano <charlieromano@gmail.com>
2015-04-13 02:04:38 -0300

    subo una version sin muchos cambios, para continuar trabajando en la validaci?n del stream de entrada. EJEMPLO: validar (I,Q)=(1,2), (-1,2), (-1,-1), (1111, 22.323), etc

--- 2015/TP0/main.cc
+++ 2015/TP0/main.cc
@@ -1,15 +1,10 @@
-// Prueba de la clase cmdline: dado un factor entero pasado por la
-// línea de comando, leemos una secuencia de números que ingresan
-// por la entrada estándar, los multiplicamos por ese factor, y
-// luego mostramos el resultado por std::cout.
-//
-// $Id: main.cc,v 1.5 2012/09/15 12:23:57 lesanti Exp $
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <cstring>
+#include <vector>
+#include <iterator>
 
 #include "common.hpp"
 #include "cmdline.h"
@@ -33,6 +28,20 @@ static option_t options[] = {
 	{0, },
 };
 
+int read_input(/*FILE &stream,*/ string str)
+{
+	cout<< "aca va la estrategia para leer y validar una entrada correcta del tipo (I,Q)"<<endl
+		<<"se me ocurre utilizar getline con delimitadores, pero hay que considerar casos como (-2,3.14159...)"<<endl;
+	return 1;
+}
+
+int validate_if_complex(string str)
+{
+	cout<<"esta funcion debe ir adentro de de read_input()"<<endl;
+	return 1;
+}
+
+
 int
 main(int argc, char * const argv[])
 {
@@ -40,12 +49,55 @@ main(int argc, char * const argv[])
 	cout << "hola" <<endl;
 	cmdline cmdl(options);
 	cmdl.parse(argc, argv);
-	string str;
+/*
+	//TEST of CMDLINE Class
+	string str, str2="(0,0)";
 	getline(*iss,str);
 	ofs << str << endl;
+	if(!str.compare(str2)) cout << "entrada OK" << endl;
+	//END TEST of CMDLINE Class
+*/
+/*
+	//TEST of COMPLEX Class
 	complejo z1(1,1);
 	cout << "La clase complejo compiló bien mirá:"<<endl;
 	z1.emitir();
+	//END TEST of COMPLEX Class
+*/
+
+	//Create a buffer of SIZE_OF_BUFFER_INPUT complex elements
+	vector<complejo> complex_vector(SIZE_OF_BUFFER_INPUT);
+	vector<complejo>::iterator complex_vector_iterator;
+
+	string aux_str;
+	read_input(/*iss,*/aux_str);
+	if(validate_if_complex(aux_str)==0){
+//		complejo aux_complex=string_to_complex(aux_str);
+	}
+	else{
+		cerr << "Error reading from input stream, is not a valid (I,Q) complex number"<<endl;
+	//Google C++ code style
+	//fprintf(stderr, "Error reading from input stream\n %s\n is not a valid (I,Q) complex number",aux_str,strerror(errno));
+
+	}
+	
+	//Show content of the vector
+	for(complex_vector_iterator=complex_vector.begin();
+		complex_vector_iterator!=complex_vector.end();
+		complex_vector_iterator++){
+		(*complex_vector_iterator).emitir();
+	}
+
+	
 	return 1;
 
 }
+
+
+
+
+
+
+
+
+
