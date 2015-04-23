4eeea4a07a775bce49a0e83506d6769108c73a12
Sebasti?n <Sebasti?n@localhost>
2015-04-23 11:22:12 -0300

    ?ltima versi?n del c?digo, ver cambios en 'Cambios.log'

---
+++ 2015/TP0/main.h
@@ -0,0 +1,19 @@
+// ------------------------------------------------------------------------- //
+// Facultad de Ingeniería de la Universidad de Buenos Aires
+// Algoritmos y Programación II
+// 1° Cuatrimestre de 2015
+// Trabajo Práctico 0: Programación en C++
+// Demodulación de señal FM
+// 
+// main.h
+// Parámetros principales de la aplicación
+//  - Tamaño de los decimadores
+// ------------------------------------------------------------------------- //
+
+#ifndef _MAIN_H_INCLUDED
+#define _MAIN_H_INCLUDED
+
+#define DECIMATOR1_SIZE 11
+#define DECIMATOR2_SIZE 4
+
+#endif
