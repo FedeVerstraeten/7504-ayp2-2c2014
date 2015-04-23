ed001bb19995c8d14d7601e531603032a59ab513
Charlie <charlieromano@gmail.com>
2015-04-23 12:33:24 -0300

    Subo cambios en el informe.

---
+++ 2015/TP0/Informe/utilities.h
@@ -0,0 +1,27 @@
+// ------------------------------------------------------------------------- //
+// Facultad de Ingeniería de la Universidad de Buenos Aires
+// Algoritmos y Programación II
+// 1° Cuatrimestre de 2015
+// Trabajo Práctico 0: Programación en C++
+// Demodulación de señal FM
+// 
+// utilities.h
+// Funciones utilitarias para el propósito de la aplicación
+//  - Imprimir en formato texto
+//  - Imprimir en formato binario U8
+// ------------------------------------------------------------------------- //
+
+
+#ifndef _UTILITIES_H_INCLUDED_
+#define _UTILITIES_H_INCLUDED_
+
+#include <iostream>
+
+#define PI 3.14159265359
+
+// Imprime en el rango [-1, 1] en formato texto (ASCII)
+void print_phase_text(double);
+// Imprime en el rango [0, 255] en formato U8 (binario)
+void print_phase_U8(double);
+
+#endif
