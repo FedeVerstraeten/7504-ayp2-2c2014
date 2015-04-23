1d36aadca08a449c4fe8e15f7cb11922b090373d
Sebasti?n <Sebasti?n@localhost>
2015-04-23 11:16:33 -0300

    ?ltima versi?n del c?digo, ver cambios en 'Cambios.log'

--- 2015/TP0/cmdline.h
+++ 2015/TP0/cmdline.h
@@ -9,31 +9,38 @@
 #define OPT_MANDATORY 2
 
 struct option_t {
-	int has_arg;
-	const char *short_name;
-	const char *long_name;
-	const char *def_value;
-	void (*parse)(std::string const &);
-	int flags;
+  int has_arg;
+  const char *short_name;
+  const char *long_name;
+  const char *def_value;
+  void (*parse)(std::string const &); // Puntero a función de opciones
+  int flags;
 };
 
-class cmdline {
-	// Este atributo apunta a la tabla que describe todas
-	// las opciones a procesar. Por el momento, sólo puede
-	// ser modificado mediante contructor, y debe finalizar
-	// con un elemento nulo.
-	//
-	option_t *option_table;
+class cmdline
+{
+  public:
+    cmdline(option_t *);
+    void parse(int, char * const []);
 
-	// El constructor por defecto cmdline::cmdline(), es
-	// privado, para evitar construir parsers sin opciones.
-	//
-	cmdline();
-	int do_long_opt(const char *, const char *);
-	int do_short_opt(const char *, const char *);
-public:
-	cmdline(option_t *);
-	void parse(int, char * const []);
-};
+  private:
+    // Este atributo apunta a la tabla que describe todas
+    // las opciones a procesar. Por el momento, sólo puede
+    // ser modificado mediante contructor, y debe finalizar
+    // con un elemento nulo.
+    //
+    option_t *option_table;
+
+    // El constructor por defecto cmdline::cmdline(), es
+    // privado, para evitar construir "parsers" (analizador 
+    // sintáctico, recibe una palabra y lo interpreta en 
+    // una acción dependiendo su significado para el programa) 
+    // sin opciones. Es decir, objetos de esta clase sin opciones.
+    //
+
+    cmdline();
+    int do_long_opt(const char *, const char *);
+    int do_short_opt(const char *, const char *);
+}; // class cmdline
 
 #endif
--- 2015/TP0/main.cc
+++ 2015/TP0/main.cc
@@ -3,9 +3,10 @@
 // Algoritmos y Programación II
 // 1° Cuatrimestre de 2015
 // Trabajo Práctico 0: Programación en C++
+// Demodulación de señal FM
 // 
-// main1.cc
-// Versión sin clase Buffer
+// main.cc
+// Archivo principal donde se ejecuta el main.
 // ------------------------------------------------------------------------- //
 
 #include <iostream>
@@ -13,88 +14,64 @@
 #include <sstream>
 #include <cstdlib>
 #include <cstring>
+
+#include "main.h"
 #include "complex.h"
 #include "cmdline.h"
+#include "arguments.h"
+#include "utilities.h"
+#include "types.h"
+
 using namespace std;
 
 
-#define DECIMATOR1_SIZE 11
-#define DECIMATOR2_SIZE 4
-
-#define FORMAT_OPTIONS 2
-#define FORMAT_TEXT "txt"
-#define FORMAT_U8 "U8"
-
-/*typedef enum{
-				
-				FORMAT_OPTION_TEXT=0,
-				FORMAT_OPTION_U8=1
-				
-} format_opt_t;*/
-
-static void opt_input(string const &);
-static void opt_output(string const &);
-//static void opt_format(string const &);
-
-static option_t options[] = {
-	{1, "i", "input", "-", opt_input, OPT_MANDATORY},
-	{1, "o", "output", "-", opt_output, OPT_MANDATORY},
-	//{1, "f", "format", "txt", opt_format, OPT_SEEN},
-	//{0, "h", "help", NULL, opt_help, OPT_DEFAULT},
-	{0, },
+// Colección de funciones para imprimir en formatos distintos
+void (*print_phase[])(double) = {
+        
+  print_phase_text,
+  print_phase_U8
+        
 };
 
-// Tienen que ser globales por algún motivo?
-// o se pueden declarar dentro del main?
-static istream *iss;
-static ostream *oss;
-//static iss = &cin;
-//static oss = &cout;
-static fstream ifs;
-static fstream ofs;
+// Opciones de argumentos de invocación
+static option_t options[] = {
+  {1, "i", "input", "-", opt_input, OPT_SEEN},
+  {1, "o", "output", "-", opt_output, OPT_SEEN},
+  {1, "f", "format", "txt", opt_format, OPT_SEEN},
+  {0, },
+};
 
-//format_opt_t format_opt = FORMAT_OPTION_TEXT;
+extern istream *iss;
+extern format_option_t format_option;
 
-/*string description_format_opt[] = {
-	
-  FORMAT_OPTION_TEXT,
-  FORMAT_OPTION_U8
-	
-};*/
-
-int main(int argc, char * const argv[])
+int
+main(int argc, char * const argv[])
 {
-  //- i := 0
-  //- j := 0
-  //- buffer1 := 0
-  //- buffer2 := 0
   size_t i = 0;
   size_t j = 0;
   Complex buffer1 = 0;
   double buffer2 = 0;
-  Complex input_complex;
   Complex x, x_prev;
   Complex aux;
   double output_phase;
-  x_prev = 0;
+
+  // Parsear argumentos de invocación
   cmdline cmdl(options);
   cmdl.parse(argc, argv);
-  
-  
 
-  //- Mientras haya complejos en la entrada
+  // Inicializar el complejo con el fomato especificado
+  Complex input_complex(format_option);
+  // cout<<int(input_complex.format_option())<<endl;
+
+  // ( -- Condiciones Iniciales Nulas -- )
+  x_prev = 0;
+
+  // Mientras haya complejos en la entrada
   while (*iss >> input_complex)
   {
-  //	(Promediar 11 elementos)
-  //	- buffer1 := buffer1 + complejo a la entrada
-  //	- Si i < 11
-  //		- i := i + 1
-  //		- Continuar con el ciclo
-  //	- x := buffer1 / 11
-  //	- buffer1 := 0
-  //	- i := 0
+    // ( -- Promediar 11 elementos -- )
     buffer1 += input_complex;
-    if (i < DECIMATOR1_SIZE-1) // Agregué el '-1', sino en vez de 11 hacía 12
+    if (i < DECIMATOR1_SIZE-1)
     {
       i++;
       continue;
@@ -103,30 +80,16 @@ int main(int argc, char * const argv[])
     buffer1 = 0;
     i = 0;
 
-
-  //	(Obtener la derivada de la fase)
-  //	- aux := x * conjugar(x_prev)
-  //	- output_phase := fase(aux)
+    // ( -- Obtener la derivada de la fase -- )
     aux = x * x_prev.conjugated();
     output_phase = aux.phase();
 
-//    cerr << "output_phase = " << output_phase << endl;
-
-  //	(Avanzar una muestra)
-  // x_prev := x
+    // ( -- Avanzar una muestra -- )
     x_prev = x;
 
-
-  //	(Promediar 4 elementos)
-  //	- buffer2 := buffer2 + output_phase
-  //	- Si j < 4
-  //		- j := j + 1
-  //		- Continuar con el ciclo
-  //	- resultado := buffer2 / 4
-  //	- buffer2 := 0
-  //	- j := 0
+    // ( -- Promediar 4 elementos -- )
     buffer2 += output_phase;
-    if (j < DECIMATOR2_SIZE-1) // Lo mismo q en el caso anterior
+    if (j < DECIMATOR2_SIZE-1)
     {
       j++;
       continue;
@@ -134,103 +97,9 @@ int main(int argc, char * const argv[])
     output_phase = buffer2/DECIMATOR2_SIZE;
     buffer2 = 0;
     j = 0;
-
-  //	Esto creo q es solo opcional, en la especifiación dice que la 
-  //  salida es (-1, 1)
-  //	(LLevar al rango requerido)
-  //	- resultado := (resultado + pi)*255/2pi 
-
-  // !!! MAL !!!, cuando hace continue en el control de flujo del
-  //              2° promediador no avanza de muestra!!
-  //	(Avanzar una muestra)
-  // x_prev := x
-//    x_prev = x;
-
-    // Esto es temporal, solo para probar:
-    //cout << output_phase/3.1416 << endl;
-	
-	*oss << (unsigned char)((output_phase + 3.14159) * 255 / (2 * 3.14159));
-	
+  
+    // ( -- Imprimir en el formato especificado -- )
+    (print_phase[format_option])(output_phase);
   }
-
-  return 0; // Se podrá usar un status_t como hacíamos en C?
-}
-
-static void
-opt_input(string const &arg)
-{
-	// Si el nombre del archivos es "-", usaremos la entrada
-	// estándar. De lo contrario, abrimos un archivo en modo
-	// de lectura.
-	//
-	if (arg == "-") {
-		iss = &cin;		// Establezco la entrada estandar cin como flujo de entrada
-	}
-	else {
-		ifs.open(arg.c_str(), ios::in); // c_str(): Returns a pointer to an array that contains a null-terminated
-										// sequence of characters (i.e., a C-string) representing
-										// the current value of the string object.
-		iss = &ifs;
-	}
-
-	// Verificamos que el stream este OK.
-	//
-	if (!iss->good()) {
-		cerr << "cannot open "
-		     << arg
-		     << "."
-		     << endl;
-		exit(1);
-	}
-}
-
-static void
-opt_output(string const &arg)
-{
-	// Si el nombre del archivos es "-", usaremos la salida
-	// estándar. De lo contrario, abrimos un archivo en modo
-	// de escritura.
-	//
-	if (arg == "-") {
-		oss = &cout;	// Establezco la salida estandar cout como flujo de salida
-	} else {
-		ofs.open(arg.c_str(), ios::out);
-		oss = &ofs;
-	}
-
-	// Verificamos que el stream este OK.
-	//
-	if (!oss->good()) {
-		cerr << "cannot open "
-		     << arg
-		     << "."
-		     << endl;
-		exit(1);		// EXIT: Terminación del programa en su totalidad
-	}
-}
-
-/*static void
-opt_format(string const &arg)
-{
-  
-  for(size_t i=0;i<FORMAT_OPTIONS;i++){
-		
-    if(arg == description_format_opt[i]){
-		
-	  format_opt=(format_opt_t)i;
-			
-	  break;
-			
-	}
-	
-  }
-	
-}*/
-
-/*void
-procces_data(const complex &input_complex)
-{
-
-  
-  
-}*/
+  return 0;
+} // main
