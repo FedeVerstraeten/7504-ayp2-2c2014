5a0274351319505382700ad29a2d5796852603cd
Sebasti?n <Sebasti?n@localhost>
2015-04-23 11:16:33 -0300

    ?ltima versi?n del c?digo, ver cambios en 'Cambios.log'

--- 2015/TP0/test4.pl
+++ 2015/TP0/test4.pl
@@ -1,11 +1,3 @@
-#! /usr/bin/perl
-#
-# test4.pl - genera una secuencia de pares complejos de entrada
-# de tal forma que la salida demodulada de la misma alterne el
-# signo.
-#
-# $Id: test4.pl,v 1.2 2015/04/05 19:18:30 lesanti Exp $
-
 $pi = abs(atan2(0, -1));
 $sign = 1.0;
 
@@ -16,4 +8,4 @@ while (1) {
 	$sign *= -1.0;
 
 	print "($re, $im)\n" x 44;
-}
+} 
