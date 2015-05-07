462f2e9bfe7253167532a6a15d73d74c74b74c5a
Rodrigo44 <rodrigobourbon44@gmail.com>
2015-05-07 10:48:19 -0300

    Actualic? un par de cosas de dft de lo que hab?amos hablado ayer

--- 2015/TP1/source/dft_methods.cc
+++ 2015/TP1/source/dft_methods.cc
@@ -10,63 +10,54 @@ using namespace std;
 
 
 Vector<Complex>
-calculate_dft(Vector<Complex> const &in)
+calculate_dft(Vector<Complex> const &x)
 {
-  Vector<Complex> out;
-  Complex W;
   Complex aux;
   size_t N;
   
-  N = in.size();
-  W = calculate_W(N);
+  N = x.size();
+  
+  Vector<Complex> X(N);
+  
+  Complex W(cos((2*PI)/N),
+            -sin((2*PI)/N));
   
   for(size_t k=0;k<N;k++){
 	
 	for(size_t n=0;n<N;n++){
 	  
-	  aux += in[n] * pow_W(n*k, W);
+	  aux += x[n] * pow_complex(W, n*k);
 	  
 	}
 	
-	out.push_back(aux);
+	X[k] = aux;
 	
 	aux = 0;
 	  
   }
   
-  return out;
+  return X;
 	
 }
 
 Complex
-calculate_W(size_t N)
-{
-
-  Complex w(cos((2*PI)/N),
-            -sin((2*PI)/N));
-  
-  return w;
-	
-}
-
-Complex
-pow_W(size_t p, Complex const &W)
+pow_complex(Complex const &z, size_t p)
 {
   
   if(!p) return 1;
   
   if(p == 1){
 	
-	return W;
+	return z;
 	
   }
   else{
 	  
-	Complex aux = pow_W(p/2, W);
+	Complex aux = pow_complex(z, p/2);
 	if(!(p%2))
       return aux * aux;
     else
-      return aux * aux * W;
+      return aux * aux * z;
   
   }
 
