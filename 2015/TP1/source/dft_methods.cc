a2e4c9e587fc390c64f3fc523c2972a8dddbc767
Rodrigo44 <rodrigobourbon44@gmail.com>
2015-05-07 12:20:15 -0300

    Hice la parte para completar la entrada a una potencia de 2, no hab?a que terminar el programa

--- 2015/TP1/source/dft_methods.cc
+++ 2015/TP1/source/dft_methods.cc
@@ -31,13 +31,117 @@ calculate_dft(Vector<Complex> const &x)
 	}
 	
 	X[k] = aux;
-	
+    
 	aux = 0;
 	  
   }
   
   return X;
 	
+}
+
+Vector<Complex>
+calculate_idft(Vector<Complex> const &X)
+{
+  
+  Complex aux;
+  size_t N;
+  
+  N = X.size();
+  
+  Vector<Complex> x(N);
+  
+  Complex W(cos((2*PI)/N),
+            -sin((2*PI)/N));
+			
+  for(size_t k=0;k<N;k++){
+	
+	for(size_t n=0;n<N;n++){
+	  
+	  aux += X[n] * (1 / pow_complex(W, n*k));
+	  
+	}
+	
+	x[k] = (1.0/N) * aux;
+    
+	aux = 0;
+	  
+  }
+  
+  return x;
+  
+}
+
+
+// Algoritmo recursivo para calcular la DFT: FFT
+Vector<Complex>
+calculate_fft(Vector<Complex> const &x)
+{
+  
+  Vector<Complex> aux_x = x;
+  size_t N;
+  size_t l;
+  N = aux_x.size();
+
+  // Si el tamaño de la entrada no es potencia de 2 se completa con ceros
+  //hasta llevarla a la potencia de 2 más cercana
+  if (log2(N) - (int)log2(N)){
+	  
+    l = (int)log2(N) + 1;
+	size_t last = my_pow(2,l) - aux_x.size();
+	
+	for(size_t i=0;i<last;i++){
+	  
+      aux_x.push_back(0);
+	  
+	}
+	
+	N = aux_x.size();
+	  
+  }
+
+  Vector<Complex> X(N);
+  
+  if (N > 1)
+  {
+    // Suponemos que la entrada es par y potencia de 2
+    Vector<Complex> p(N/2);
+    Vector<Complex> q(N/2);
+    Vector<Complex> P(N/2);
+    Vector<Complex> Q(N/2);
+    for (size_t i=0; i<N/2; i++)
+    {
+      p[i] = aux_x[2*i];
+      q[i] = aux_x[2*i+1];
+    }
+ 
+    P = calculate_fft(p);
+    Q = calculate_fft(q);
+    
+    for (size_t k=0; k<N; k++)
+    {
+      Complex W(cos(k*(2*PI)/N),
+               -sin(k*(2*PI)/N));
+      // Para que se repitan los elementos cíclicamente, se utiliza la función módulo
+      size_t k2 = k % (N/2);
+
+      X[k] = P[k2] + W*Q[k2];
+    } 
+  }
+  else
+  {
+    X = aux_x;
+  }
+  
+  return X;
+}
+
+  
+
+Vector<Complex>
+calculate_ifft(Vector<Complex> const &X)
+{
+  return X;
 }
 
 Complex
@@ -63,66 +167,26 @@ pow_complex(Complex const &z, size_t p)
 
 }
 
-
-Vector<Complex>
-calculate_idft(Vector<Complex> const &X)
+size_t
+my_pow(size_t const &n, size_t p)
 {
-  return X;
-}
-
-
-// Algoritmo recursivo para calcular la DFT: FFT
-Vector<Complex>
-calculate_fft(Vector<Complex> const &x)
-{
-  size_t N;
-  N = x.size();
-
-  // Si el tamaño de la entrada no es potencia de 2 se termina el programa
-  if (log2(N) - (int)log2(N))
-    exit(1);
-
-  Vector<Complex> X(N);
   
-  if ( N > 1)
-  {
-    // Suponemos que la entrada es par y potencia de 2
-    Vector<Complex> p(N/2);
-    Vector<Complex> q(N/2);
-    Vector<Complex> P(N/2);
-    Vector<Complex> Q(N/2);
-    for (size_t i=0; i<N/2; i++)
-    {
-      p[i] = x[2*i];
-      q[i] = x[2*i+1];
-    }
- 
-    P = calculate_fft(p);
-    Q = calculate_fft(q);
-    
-    for (size_t k=0; k<N; k++)
-    {
-      Complex W(cos(k*(2*PI)/N),
-               -sin(k*(2*PI)/N));
-      // Para que se repitan los elementos cíclicamente, se utiliza la función módulo
-      size_t k2 = k % (N/2);
-
-      X[k] = P[k2] + W*Q[k2];
-    } 
+  if(!p) return 1;
+  
+  if(p == 1){
+	
+	return n;
+	
   }
-  else
-  {
-    X = x;
+  else{
+	  
+	size_t aux = my_pow(n, p/2);
+	if(!(p%2))
+      return aux * aux;
+    else
+      return aux * aux * n;
+  
   }
-  
-  return X;
-}
 
-  
-
-Vector<Complex>
-calculate_ifft(Vector<Complex> const &X)
-{
-  return X;
 }
 
