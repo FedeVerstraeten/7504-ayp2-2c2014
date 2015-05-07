8f455dd47d32eeeac363a7b122d2b211ec885674
Charlie <charlieromano@gmail.com>
2015-05-07 18:51:57 -0300

    Puse las funciones pow en dft_methods porque las borraron para ponerlas en utilities.h pero nunca subieron utilities.h y no compilaba. Ahora compila y prob? con un vector ok

--- 2015/TP1/source/dft_methods.h
+++ 2015/TP1/source/dft_methods.h
@@ -6,11 +6,15 @@
 #include "complex.h"
 #include "vector.h"
 
-#define PI 3.14159
+//#define PI 3.14159 definida en utilities.h
 
 Vector<Complex> calculate_dft(Vector<Complex> const &);
 Vector<Complex> calculate_idft(Vector<Complex> const &);
 Vector<Complex> calculate_fft(Vector<Complex> const &);
 Vector<Complex> calculate_ifft(Vector<Complex> const &);
+Vector<Complex> calculate_fft_iter(Vector<Complex> const &);
+Vector<Complex> calculate_ifft_iter(Vector<Complex> const &);
+Complex pow_complex(Complex const &, size_t);
+size_t my_pow(size_t const &, size_t);
 
 #endif
