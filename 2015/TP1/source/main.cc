5e8db1f5bb79ebdd5672d2d49eb771c34d095c65
Sebasti?n <Sebasti?n@localhost>
2015-05-07 07:53:28 -0300

    Implement? la funci?n fft y funciona.

--- 2015/TP1/source/main.cc
+++ 2015/TP1/source/main.cc
@@ -2,15 +2,12 @@
 #include <cstdlib>
 #include <sstream>
 
-
 #include "main.h"
 #include "cmdline.h"
 #include "arguments.h"
 #include "complex.h"
 #include "vector.h"
-// Esto se puede cambiar por:
-// dft_methods.h
-// porque en definitiva lo que calculamos en todos los casos es la DFT
+// En definitiva lo que calculamos en todos los casos es la DFT
 // (incluso en el caso de la fft, es un algorítmo para calcular la DFT)
 #include "dft_methods.h"
 #include "types.h"
@@ -18,14 +15,13 @@
 using namespace std;
 
 
-// en vez de "transform", puede ser "transformate" directamente
+
 Vector<Complex> (*transform[])(Vector<Complex> const &) = {
-	
-    // o bien calculate_dft más directo
+
     calculate_dft,
-	//calculate_idft,
-	//calculate_fft,
-	//calculate_ifft
+	  calculate_idft,
+    calculate_fft,
+	  calculate_ifft
 
 };
 
@@ -33,8 +29,8 @@ extern option_t options[];
 extern istream *iss;
 extern method_option_t method_option;
 
-int main(int argc, char *argv[]){
-	
+int main(int argc, char *argv[])
+{	
   Complex input_complex;
   Vector<Complex> input;
   Vector<Complex> output;
