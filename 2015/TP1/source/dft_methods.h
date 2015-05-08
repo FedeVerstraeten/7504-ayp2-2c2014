d5fdbcc9e3507f92dfdea233377b3d7794d42f07
Rodrigo44 <rodrigobourbon44@gmail.com>
2015-05-08 16:14:00 -0300

    Ahora s? subo los archivos como corresponde

--- 2015/TP1/source/dft_methods.h
+++ 2015/TP1/source/dft_methods.h
@@ -6,15 +6,11 @@
 #include "complex.h"
 #include "vector.h"
 
-//#define PI 3.14159 definida en utilities.h
+#define PI 3.14159
 
 Vector<Complex> calculate_dft(Vector<Complex> const &);
 Vector<Complex> calculate_idft(Vector<Complex> const &);
 Vector<Complex> calculate_fft(Vector<Complex> const &);
 Vector<Complex> calculate_ifft(Vector<Complex> const &);
-Vector<Complex> calculate_fft_iter(Vector<Complex> const &);
-Vector<Complex> calculate_ifft_iter(Vector<Complex> const &);
-Complex pow_complex(Complex const &, size_t);
-size_t my_pow(size_t const &, size_t);
 
 #endif
--- 2015/TP1/source/utilities.h
+++ 2015/TP1/source/utilities.h
@@ -1,27 +1,11 @@
-// ------------------------------------------------------------------------- //
-// Facultad de Ingenieria de la Universidad de Buenos Aires
-// Algoritmos y Programacion II
-// 1er Cuatrimestre de 2015
-// Trabajo Practico 0: Programacion en C++
-// Demodulacion de senal FM
-// 
-// utilities.h
-// Funciones utilitarias para el proposito de la aplicacion
-//  - Imprimir en formato texto
-//  - Imprimir en formato binario U8
-// ------------------------------------------------------------------------- //
-
-
 #ifndef _UTILITIES_H_INCLUDED_
 #define _UTILITIES_H_INCLUDED_
 
 #include <iostream>
 
-#define PI 3.14159265359
+#include "complex.h"
 
-// Imprime en el rango [-1, 1] en formato texto (ASCII)
-void print_phase_text(double);
-// Imprime en el rango [0, 255] en formato U8 (binario)
-void print_phase_U8(double);
+Complex pow_complex(Complex const &, size_t);
+size_t my_pow(size_t const &, size_t);
 
 #endif
