8738737d63af175e3f25d1a729723a42ef3f0960
Sebasti?n <Sebasti?n@localhost>
2015-05-20 05:36:33 -0300

    Agregu? la parte de compilaci?n y actualic? el 'TO DO.txt'. Reemplac? TABs por SPACEs en algunos c?digos

--- 2015/TP1/source/arguments.cc
+++ 2015/TP1/source/arguments.cc
@@ -24,21 +24,21 @@ using namespace std;
 
 // Opciones de argumentos de invocacion
 option_t options[] = {
-	{1, "i", "input", "-", opt_input, OPT_SEEN},
-	{1, "o", "output", "-", opt_output, OPT_SEEN},
-	{1, "m", "method", "fft", opt_method, OPT_SEEN},
-	{0, },
+  {1, "i", "input", "-", opt_input, OPT_SEEN},
+  {1, "o", "output", "-", opt_output, OPT_SEEN},
+  {1, "m", "method", "fft", opt_method, OPT_SEEN},
+  {0, },
 };
 
 // Nombres de los argumentos de la opcion "--method"
 string description_method_option[] = {
-	
+  
     METHOD_DFT,
     METHOD_IDFT,
     METHOD_FFT,
     METHOD_IFFT,
     METHOD_FFT_ITER,
-    METHOD_IFFT_ITER	
+    METHOD_IFFT_ITER  
 };
 
 istream *iss;
@@ -50,54 +50,54 @@ method_option_t method_option;
 void
 opt_input(string const &arg)
 {
-	// Si el nombre del archivos es "-", usaremos la entrada
-	// estándar. De lo contrario, abrimos un archivo en modo
-	// de lectura.
-	//
-	if (arg == "-") {
-		iss = &cin;		// Establezco la entrada estandar cin como flujo de entrada
-	}
-	else {
-		ifs.open(arg.c_str(), ios::in); // c_str(): Returns a pointer to an array that contains a null-terminated
-										// sequence of characters (i.e., a C-string) representing
-										// the current value of the string object.
-		iss = &ifs;
-	}
+  // Si el nombre del archivos es "-", usaremos la entrada
+  // estándar. De lo contrario, abrimos un archivo en modo
+  // de lectura.
+  //
+  if (arg == "-") {
+    iss = &cin;    // Establezco la entrada estandar cin como flujo de entrada
+  }
+  else {
+    ifs.open(arg.c_str(), ios::in); // c_str(): Returns a pointer to an array that contains a null-terminated
+                    // sequence of characters (i.e., a C-string) representing
+                    // the current value of the string object.
+    iss = &ifs;
+  }
 
-	// Verificamos que el stream este OK.
-	//
-	if (!iss->good()) {
-		cerr << "Cannot open "
-		     << arg
-		     << "."
-		     << endl;
-		exit(1);
-	}
+  // Verificamos que el stream este OK.
+  //
+  if (!iss->good()) {
+    cerr << "Cannot open "
+         << arg
+         << "."
+         << endl;
+    exit(1);
+  }
 }
 
 void
 opt_output(string const &arg)
 {
-	// Si el nombre del archivos es "-", usaremos la salida
-	// estándar. De lo contrario, abrimos un archivo en modo
-	// de escritura.
-	//
-	if (arg == "-") {
-		oss = &cout;	// Establezco la salida estandar cout como flujo de salida
-	} else {
-		ofs.open(arg.c_str(), ios::out);
-		oss = &ofs;
-	}
+  // Si el nombre del archivos es "-", usaremos la salida
+  // estándar. De lo contrario, abrimos un archivo en modo
+  // de escritura.
+  //
+  if (arg == "-") {
+    oss = &cout;  // Establezco la salida estandar cout como flujo de salida
+  } else {
+    ofs.open(arg.c_str(), ios::out);
+    oss = &ofs;
+  }
 
-	// Verificamos que el stream este OK.
-	//
-	if (!oss->good()) {
-		cerr << "Cannot open "
-		     << arg
-		     << "."
-		     << endl;
-		exit(1);		// EXIT: Terminación del programa en su totalidad
-	}
+  // Verificamos que el stream este OK.
+  //
+  if (!oss->good()) {
+    cerr << "Cannot open "
+         << arg
+         << "."
+         << endl;
+    exit(1);    // EXIT: Terminación del programa en su totalidad
+  }
 }
 
 void
@@ -108,7 +108,7 @@ opt_method(string const &arg)
   for(i=0; i < METHOD_OPTIONS; i++) {
     if(arg == description_method_option[i]) {
       method_option = (method_option_t)i; // Casteo
-      break;	
+      break;  
     }  
   }
   // Si recorrio todo el diccionario, el argumento no esta implementado
